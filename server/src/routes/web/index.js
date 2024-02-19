e47cb77e5a8c78bc5a0fe88390faaa3d968be895
bachtv1509 <bachtv150902@gmail.com>
2024-02-19 12:21:39 +0700

    .

--- server/src/routes/web/index.js
+++ server/src/routes/web/index.js
@@ -8,10 +8,11 @@ import { userRouter } from './user';
 export const webRouter = express.Router();
 
 webRouter.get('/', (req, res) => {
-  res.json('Welcome to coursera replica');
+    res.json('Welcome to coursera replica');
 });
 webRouter.use('/notify', notifyRouter);
 webRouter.use('/post', postRouter);
 webRouter.use('/course', courseRouter);
 webRouter.use('/comment', commentRouter);
 webRouter.use('/user', userRouter);
+
